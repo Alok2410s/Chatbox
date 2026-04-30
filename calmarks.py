6f1df9109408451ba441a4f8cb195d5540ff8a98
Alok2410s <ak0144709@gmail.com>
2026-04-30 10:42:53 +0530

    add initial files for programming concepts and calculations

---
+++ calmarks.py
@@ -0,0 +1,5 @@
+# Calculate marks manually
+
+marks = [85, 90, 78, 92, 88]
+print (sum(marks) / len(marks))
+
---
+++ smallcode.py
@@ -0,0 +1,47 @@
+#print("Hello, World!")
+
+# learning python
+
+# name=input("Enter your name: ")
+# print("hola", name)
+
+# prerequisite means something that is required as a prior condition for something else to happen or exist.
+# In the context of learning Python, it refers to the foundational knowledge or skills that are necessary before
+# you can effectively learn and use Python programming language. This may include understanding basic programming
+# concepts, familiarity with computer science principles, and having a general understanding of how programming languages work.
+
+### Odd or Even
+# num =int(input("Enter a number: "))
+# if (num%2==0):
+#     print("Even number is", num)
+# else:
+#     print("Odd number is", num)
+    
+### first Largest 
+a,b,c = 10,30, 40
+if (a>b):
+    print("a is greater than b")
+elif (b>c):
+    print("b is greater than c")
+
+### using syntax
+for i in range(5):
+    print(i)
+
+### scratch uses blocks instead of typing code.
+
+for i in range(5):
+    print("Move")
+
+###loops repeat
+   
+for i in range(2,6):
+    print(i)
+
+###More about loops
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
