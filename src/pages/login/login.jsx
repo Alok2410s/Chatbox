bf2c413ecdc3f7936d6d45c1a7b0223405d0a179
Alok kumar <ak0144709@gmail.com>
2026-05-15 15:13:26 +0530

    feat: implement login page layout and styling with form inputs and logo

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Login.css'
-
+import assets from '../../assets/assets'
 const Login = () => {
     return (
-        <div>
-            we are on the chat page
+        <div className='login'>
+            <img src={assets.logo_big} alt="" className="logo" />
+            <form className="login-form">
+                <h2>
+                    Sign Up
+                </h2>
+                <input type="text" placeHolder='username' className="form-input" required />
+                <input type="email" placeHolder='Email address' className="form-input" required />
+                <input type="password" placeHolder='password' className="form-input" required />
+                <button type='summit'>Sign Up</button>
+                <div className="login-term">
+                    <imput type="checkbox" />
+                    <p>Agree to the terms of use & privacy policy.</p>
+                </div>
+                <div className="login-forget">
+                    <p  className="login-toggle">Already have an account <span>click here</span></p>
+                </div>
+            </form>
         </div>
     )
 }
