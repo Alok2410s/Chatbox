13f38983e3607489e5d0fc7610f279a0e486035b
Alok kumar <ak0144709@gmail.com>
2026-05-15 15:34:58 +0530

    feat: enhance login component to toggle between sign up and login states

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,23 +2,31 @@ import React from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 const Login = () => {
+
+    const Login = () =>{
+        const [currstate,setCurrState] = useState("Sign up");
+    }
     return (
         <div className='login'>
             <img src={assets.logo_big} alt="" className="logo" />
             <form className="login-form">
                 <h2>
-                    Sign Up
+                   {currState}
                 </h2>
-                <input type="text" placeHolder='username' className="form-input" required />
+                {currState === "Sign up"? <input type="text" placeHolder='username' className="form-input" required />: null}
                 <input type="email" placeHolder='Email address' className="form-input" required />
                 <input type="password" placeHolder='password' className="form-input" required />
-                <button type='summit'>Sign Up</button>
+                <button type='summit'>{currState === "Sign up"?"Create account":"Login"}</button>
                 <div className="login-term">
                     <imput type="checkbox" />
                     <p>Agree to the terms of use & privacy policy.</p>
                 </div>
                 <div className="login-forget">
-                    <p  className="login-toggle">Already have an account <span>click here</span></p>
+                    {
+                        currState ==="Sign up"
+                        ?<p  className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+                        :<p  className="login-toggle">Create an account <span onClick={()=>setCurrState("Sign up")}>click here</span></p>
+                    }
                 </div>
             </form>
         </div>
