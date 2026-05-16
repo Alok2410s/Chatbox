bbee4e56b54ee1d1715d6e4fce0bf6cd18903188
Alok kumar <ak0144709@gmail.com>
2026-05-16 21:24:10 +0530

    feat: add Chat, Chatbox, Leftsidebar, and Rightsidebar components with initial structure and styling

---
+++ src/components/Leftsidebar/Leftsidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Leftsidebar.css'
+
+const Leftsidebar = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Leftsidebar
---
+++ src/components/Rightsidebar/Rightsidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Rightsidebar.css'
+
+const Rightsidebar = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Rightsidebar
